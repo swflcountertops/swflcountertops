db6b3a54eaeb29bbcfe4cbb53eccf7b565a63005
swflcountertops <general@swflcountertops.com>
2026-07-03 22:28:03 -0400

    Delete swflcountertops_website_files directory

--- swflcountertops_website_files/README.md
+++
@@ -1,9 +0,0 @@
-# SWFL Countertops LLC Website
-
-Files:
-- `index.html`
-- `style.css`
-- `script.js`
-- `assets/logo.png` placeholder
-
-Replace `assets/logo.png` with your real logo file using the same name.
--- swflcountertops_website_files/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(a => {
-  a.addEventListener('click', e => {
-    const id = a.getAttribute('href');
-    const el = document.querySelector(id);
-    if (el) { e.preventDefault(); el.scrollIntoView({behavior:'smooth'}); }
-  });
-});
